d2a6ab41f35f67841ef9788bf618cd3bf22eb6aa
wrrnwright-eagles <warren.wright@eagles.oc.edu>
2023-06-27 18:43:10 -0500

    updates to admindashboard

--- src/views/AdminDashboard.vue
+++ src/views/AdminDashboard.vue
@@ -6,9 +6,54 @@ import CustomerServices from "../services/CustomerServices.js";
 
 const couriers = ref([]);
 const customers = ref([]);
+const isAdmin = ref(false);
+
+onMounted(async () => {
+  await getCustomers();
+  await getCouriers();
+});
+
+
+async function getCustomers() {
+    try {
+    const response = await CustomerServices.getCustomer(route.params.id);
+    console.log(response.data);
+    customers.value = response.data[0];
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Add, Update and Remove Customers
+
+async function getCouriers() {
+    try {
+    const response = await CourierServices.getCourier(route.params.id);
+    console.log(response.data);
+    couriers.value = response.data[0];
+  } catch (error) {
+    console.log(error);
+  }
+
+}
+
+// Add, Update and Remove Couriers
+
+
+
+
 
 </script>
 
 <template>
+    <v-container>
+        <v-row align="center">
+            <v-col cols="10">
+                <v-card-title class="pl-0 text-h4 font-weight-bold">
+                Dashboard
+                </v-card-title>
+            </v-col>
+        </v-row>
+    </v-container>
 
 </template>
