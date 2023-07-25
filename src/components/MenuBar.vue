58e461dca7e4ea8da53d4452bef1a8c2aa6d7345
Nelson Bucyibaruta <nelsoruta@gmail.com>
2023-07-24 23:47:47 -0400

    nelson menu-change

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -50,34 +50,30 @@ function logout() {
       <v-btn v-if="user !== null && user.isClerk" class="mx-2" :to="{ name: 'clerkdashboard' }">
         Dashboard
       </v-btn>
-      <v-btn v-if="user !== null" class="mx-2" :to="{ name: 'map' }">
-        Map
-      </v-btn>
       <v-menu v-if="user !== null" min-width="200px" rounded>
-        <template v-slot:activator="{ props }">
-          <v-btn icon v-bind="props">
-            <v-avatar class="mx-auto text-center" color="green" size="large">
-              <span class="white--text font-weight-bold">{{
-                `${user.firstName.charAt(0)}${user.lastName.charAt(0)}`
-              }}</span>
+      <template v-slot:activator="{ props }">
+        <v-btn icon v-bind="props">
+          <v-avatar class="mx-auto text-center" color="green" size="large">
+            <span class="white--text font-weight-bold">{{ user.profilePicture ? '' : `${user.firstName.charAt(0)}${user.lastName.charAt(0)}` }}</span>
+            <img v-if="user.profilePicture" :src="user.profilePicture" alt="Profile Picture" />
+          </v-avatar>
+        </v-btn>
+      </template>
+      <v-card>
+        <v-card-text>
+          <div class="mx-auto text-center">
+            <v-avatar color="green">
+              <span class="white--text text-h5">{{ user.profilePicture ? '' : `${user.firstName.charAt(0)}${user.lastName.charAt(0)}` }}</span>
+              <img v-if="user.profilePicture" :src="user.profilePicture" alt="Profile Picture" />
             </v-avatar>
-          </v-btn>
-        </template>
-        <v-card>
-          <v-card-text>
-            <div class="mx-auto text-center">
-              <v-avatar color="green">
-                <span class="white--text text-h5">{{
-                  `${user.firstName.charAt(0)}${user.lastName.charAt(0)}`
-                }}</span>
-              </v-avatar>
-              <h3>{{ `${user.firstName} ${user.lastName}` }}</h3>
-              <p class="text-caption mt-1">
-                {{ user.email }}
-              </p>
-              <v-divider class="my-3"></v-divider>
-              <v-btn rounded variant="text" @click="logout()"> Logout </v-btn>
-            </div>
+            <h3>{{ `${user.firstName} ${user.lastName}` }}</h3>
+            <p class="text-caption mt-1">
+              {{ user.email }}
+            </p>
+            <input type="file" @change="handleProfilePictureUpload" accept="image/*" />
+            <v-divider class="my-3"></v-divider>
+            <v-btn rounded variant="text" @click="logout()">Logout</v-btn>
+          </div>
           </v-card-text>
         </v-card>
       </v-menu>
